c91556f8a8e2a77624679ccfc82b33f602a3c29b
lipengfei <lipfd@yonyou.com>
2018-11-22 11:40:53 +0800

    添加扩展名配置

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-import './styles/common.css';
+import './styles/common';
+
+function component() {
+  var element = document.createElement('div');
+
+  element.innerHTML = 'Hello, Webpack!';
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: resolve('dist')
+  },
+  resolve: {
+    // 扩展名:配置之后引用文件可以省略
+    extensions: [' ', '.js', '.jsx', '.vue', '.json', '.css']
   },
   module: {
     rules: [
