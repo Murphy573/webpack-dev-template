7bdfd7110e1da487183ed6e3aca6fceadf28966f
lipengfei <lipfd@yonyou.com>
2018-11-23 10:05:23 +0800

    添加webpack-dev-server

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import './styles/common';
+import './assets/iconfont/iconfont';
+import LYF from './assets/lyf.jpg';
+import { A } from './scripts/temp';
 
 function component() {
-  var element = document.createElement('div');
+  let element = document.createElement('div');
 
   element.innerHTML = 'Hello, Webpack!';
   element.classList.add('hello');
+
+  let elmI = document.createElement('i');
+  elmI.classList.add('iconfont', 'icon-tuangouguanli');
+  document.body.appendChild(elmI);
+
+  let lyf = new Image();
+  lyf.src = LYF;
+
+  element.appendChild(lyf);
 
   return element;
 }
 
 document.body.appendChild(component());
+A();
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,107 @@
 const path = require('path');
 const resolve = dir => path.join(__dirname, dir);
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+console.log(__dirname);
 module.exports = {
+  // mode: 'development',
+  context: resolve(''),
+  node: {
+    setImmediate: false,
+    process: 'mock',
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  },
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
-    path: resolve('dist')
+    filename: 'static/js/[name].[contenthash:8].js',
+    path: resolve('dist'),
+    publicPath: '/'
   },
   resolve: {
     // 扩展名:配置之后引用文件可以省略
     extensions: [' ', '.js', '.jsx', '.vue', '.json', '.css']
   },
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'static/img/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'static/media/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(svg)(\?.*)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'static/fonts/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'static/fonts/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+      inject: true
+    })
+  ]
 };
